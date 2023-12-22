f608915b40daf3d50ff25f0752b39518de0cf379
AppSiDaL <giliecrack343@gmail.com>
2023-12-21 21:15:07 -0600

    Update prediction and training scripts

--- backend/proto/prediction.ts
+++ backend/proto/prediction.ts
@@ -1,29 +1,56 @@
-import * as tf from '@tensorflow/tfjs-node';
+import * as tf from "@tensorflow/tfjs-node";
+import { SQLTesjoResponse } from "../types";
+const { Weather } = require("../models");
 
 async function predict() {
   // Cargar el modelo.
-  const model = await tf.loadLayersModel('file://my-model/model.json');
+  const model = await tf.loadLayersModel("file://.//model.json");
+  const results: SQLTesjoResponse[] = await Weather.findAll({
+    limit: 1,
+    order: [["timestamp", "DESC"]],
+  });
 
-  // Crear un tensor de entrada para la predicción.
-  // Asegúrate de que estos valores están en el formato correcto y normalizados de la misma manera que los datos de entrenamiento.
-  const input = tf.tensor2d([[
-    Number('timestamp'),
-    Number('hora'),
-    Number('minuto'),
-    Number('direccion'),
-    Number('humedad'),
-    Number('lluvia'),
-    Number('luz'),
-    Number('presion'),
-    Number('temperatura'),
-    Number('velocidad'),
-  ]]);
+  if (results.length === 0) {
+    console.error("No hay datos disponibles para hacer predicciones.");
+    return;
+  }
 
-  // Hacer una predicción con el modelo.
-  const output = model.predict(input) as tf.Tensor;
+  // Obtener datos de entrada y normalizarlos
+  const inputData = [
+    Number(results[0].dataValues.timestamp),
+    Number(results[0].dataValues.hora),
+    Number(results[0].dataValues.minuto),
+    Number(results[0].dataValues.direccion),
+    Number(results[0].dataValues.humedad),
+    Number(results[0].dataValues.lluvia),
+    Number(results[0].dataValues.luz),
+    Number(results[0].dataValues.presion),
+    Number(results[0].dataValues.temperatura),
+    Number(results[0].dataValues.velocidad),
+  ];
 
-  // Imprimir la predicción.
-  output.print();
+  // Normalizar los datos de entrada directamente
+  const normalizedInput = tf.tensor2d([inputData]).div(1000); // Ajusta este valor según sea necesario
+
+  // Hacer la predicción
+  const output:any = model.predict(normalizedInput) as tf.Tensor;
+
+  // Desnormalizar los resultados (ajusta según tu proceso de entrenamiento)
+  const outputData = output
+    .mul(1000) // Ajusta este valor según sea necesario
+    .arraySync()[0];
+
+  // Imprimir los resultados desnormalizados
+  console.log(`Timestamp: ${outputData[0]}`);
+  console.log(`Hora: ${outputData[1]}`);
+  console.log(`Minuto: ${outputData[2]}`);
+  console.log(`Dirección: ${outputData[3]}`);
+  console.log(`Humedad: ${outputData[4]}`);
+  console.log(`Lluvia: ${outputData[5]}`);
+  console.log(`Luz: ${outputData[6]}`);
+  console.log(`Presión: ${outputData[7]}`);
+  console.log(`Temperatura: ${outputData[8]}`);
+  console.log(`Velocidad: ${outputData[9]}`);
 }
 
 predict();
--- backend/proto/train.ts
+++ backend/proto/train.ts
@@ -1,50 +1,70 @@
-import { TesjoValues } from "../types";
 import * as tf from "@tensorflow/tfjs-node";
 const { Weather } = require("../models");
 
-// Crear un modelo secuencial.
+// Define el modelo
 const model = tf.sequential();
+model.add(tf.layers.dense({ units: 64, activation: "relu", inputShape: [10] }));
+model.add(tf.layers.dense({ units: 10, activation: "linear" }));
 
-// Añadir una capa densa (fully connected) al modelo.
-model.add(
-  tf.layers.dense({ units: 100, activation: "relu", inputShape: [10] })
-);
-model.add(tf.layers.dense({ units: 1 }));
-
-// Compilar el modelo.
+// Compila el modelo
 model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
 
-// Hacer una consulta a la base de datos.
+// Carga los datos
 Weather.findAll({})
   .then((results: any) => {
-    // Convertir los resultados en tensores.
-    // Convertir los resultados en tensores.
     const xs = tf.tensor2d(
-      results.map((result: TesjoValues) => [
-        Number(result.timestamp),
-        Number(result.hora),
-        Number(result.minuto),
-        Number(result.direccion),
-        Number(result.humedad),
-        Number(result.lluvia),
-        Number(result.luz),
-        Number(result.presion),
-        Number(result.temperatura),
-        Number(result.velocidad),
+      results.map((result: any) => [
+        Number(result.dataValues.timestamp),
+        Number(result.dataValues.hora),
+        Number(result.dataValues.minuto),
+        Number(result.dataValues.direccion),
+        Number(result.dataValues.humedad),
+        Number(result.dataValues.lluvia),
+        Number(result.dataValues.luz),
+        Number(result.dataValues.presion),
+        Number(result.dataValues.temperatura),
+        Number(result.dataValues.velocidad),
       ])
     );
-    const ys = tf.tensor2d(results.map((result: any) => [result.target]));
+    const ys = tf.tensor2d(
+      results.map((result: any) => [
+        Number(result.dataValues.timestamp),
+        Number(result.dataValues.hora),
+        Number(result.dataValues.minuto),
+        Number(result.dataValues.direccion),
+        Number(result.dataValues.humedad),
+        Number(result.dataValues.lluvia),
+        Number(result.dataValues.luz),
+        Number(result.dataValues.presion),
+        Number(result.dataValues.temperatura),
+        Number(result.dataValues.velocidad),
+      ])
+    );
 
-    // Entrenar el modelo.
-    model.fit(xs, ys, {
+    // Normaliza los datos
+    const xsMax = xs.max();
+    const xsMin = xs.min();
+    const ysMax = ys.max();
+    const ysMin = ys.min();
+
+    const normalizedXS = xs.sub(xsMin).div(xsMax.sub(xsMin));
+    const normalizedYS = ys.sub(ysMin).div(ysMax.sub(ysMin));
+
+    // Entrena el modelo
+    return model.fit(normalizedXS, normalizedYS, {
       epochs: 100,
       callbacks: {
         onEpochEnd: async (epoch: any, log: any) => {
-          console.log(`Epoch ${epoch}: loss = ${log.loss}`);
+          console.log(`Epoch ${epoch}: log: ${log.loss}`);
         },
       },
     });
   })
-  .then(async () => {
-    await model.save("file://my-model");
+  .then(async (info: any) => {
+    console.log("Training Complete", info);
+    // Guarda el modelo
+    await model.save("file://./");
+  })
+  .catch((err: any) => {
+    console.error(err);
   });
